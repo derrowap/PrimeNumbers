1e46d8cedabff62c73a4331b9303d92cd88a9969
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-02-21 18:40:19 -0500

    finished brute force method of primes
    Brute force will print out to console all primes it finds

--- BruteForce.c
+++ BruteForce.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-	int j, totalCount = 1, primes[100] = {2};
-	char isPrime= 1;
+#define PRIME_COUNT 500
+
+bool prime(int p) {
+	int i;
+	for(i = 2; i < p; i++) {
+		if(p % i == 0) return false;
+	}
+	return true;
+}
+
+void bruteForcePrimes() {
+	int totalCount = 2, primes[PRIME_COUNT] = {2, 3};
 	long i;
-	for (i = 3; primes[100] == 0; i+=2) {
-		for (j = 3; j * j <= i; j += 2) {
-			if ((i % j) == 0) {
-				isPrime = 0; 
-				break;
-			}
-		}
-		if (isPrime == 1) {
+	for (i = 4; primes[PRIME_COUNT - 1] == 0; i += 1) {
+		if(prime(i)) {
 			primes[totalCount] = i;
 			totalCount++;
 		}
-		else isPrime = 1;
 	} 
-	printf("total: %d\n", totalCount);
+	printf("total primes found: %d\n", totalCount);
 	for(i = 0; i < totalCount; i++) {
+		if(i % 10 == 0 && i != 0) printf("\n");
 		printf("%d\t", primes[i]);
-		if(i % 10 == 0) printf("\n");
 	}
+}
 
+int main() {
+	bruteForcePrimes();
 	return 0;
 }
